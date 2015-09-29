b3adb5f5deb3356a54e403cfe29c0e1e342c28d6
applebrian <apple800@hotmail.com>
2015-09-29 12:44:00 -0700

    update

---
+++ DateValidation/DateValidation/DateValidation.h
@@ -0,0 +1,15 @@
+//
+//  DateValidation.h
+//  DateValidation
+//
+//  Created by Brian Chen on 9/28/15.
+//  Copyright © 2015 Brian Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DateValidation : NSObject
+
+- (NSString *)changeToStandardTime:(NSTimeInterval)timeInterval;
+
+@end
